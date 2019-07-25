be7ce32587ce040eeb11860689196d30452a9cc4
vctls <toulousevictor@gmail.com>
2019-07-25 10:25:48 +0200

    Adds fetch style parameter.

--- src/web/index.php
+++ src/web/index.php
@@ -3,6 +3,9 @@
 // Copy env.php.dist into env.php and change your connection settings.
 include '../cfg/env.php';
 
+const FETCH_STYLE = 'fetch_style';
+const QUERY = 'query';
+
 $pdo = new PDO(
     "informix:host=$host;" .
     "service=$port;" .
@@ -14,17 +17,23 @@ $pdo = new PDO(
     $pwd
 );
 
-if (array_key_exists('query', $_GET)) {
-    $sql = $_GET['query'];
+if (array_key_exists(QUERY, $_GET)) {
+    $sql = $_GET[QUERY];
 } else {
     $sql = 'SELECT CURRENT FROM systables WHERE tabid=1';
+}
+
+if (array_key_exists(FETCH_STYLE, $_GET)) {
+    $fetch_style = $_GET[FETCH_STYLE];
+} else {
+    $fetch_style = PDO::FETCH_ASSOC;
 }
 
 // Enable exceptions on PDO errors.
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $stm = $pdo->query($sql);
-$results = $stm->fetchAll(PDO::FETCH_ASSOC);
+$results = $stm->fetchAll($fetch_style);
 $response = json_encode($results, JSON_INVALID_UTF8_IGNORE);
 $json_error = json_last_error() . ' ' . json_last_error_msg();
 
